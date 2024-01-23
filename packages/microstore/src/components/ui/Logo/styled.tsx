398fe508ad1dbcd27034900956dfa01bdca53b8a
helua <helua@users.noreply.github.com>
2024-01-23 12:40:33 +0100

    undo logo width change

--- packages/microstore/src/components/ui/Logo/styled.tsx
+++ packages/microstore/src/components/ui/Logo/styled.tsx
@@ -2,7 +2,7 @@ import Styled from "styled-components"
 import tw from "twin.macro"
 
 export const Image = Styled.img`
-  ${tw`w-auto max-w-full`}
+  ${tw`w-60 max-w-full`}
 `
 export const Label = Styled.h1`
   ${tw`font-extrabold uppercase tracking-wide text-xl text-black`}
