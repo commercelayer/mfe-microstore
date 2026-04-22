c5f25828f35e47cc30ba6ad665fedc93de235469
Berardo Matè <berardo@commercelayer.io>
2026-04-22 11:16:09 +0200

    fix: add variant label string

--- packages/microstore/src/components/composite/Product/VariantSelector/index.tsx
+++ packages/microstore/src/components/composite/Product/VariantSelector/index.tsx
@@ -1,4 +1,5 @@
 import { type ChangeEvent, type FC, useState } from "react"
+import { useTranslation } from "react-i18next"
 import { Label } from "#components/ui/Label"
 import { Select } from "#components/ui/Select"
 import type { SkuWithPrices } from "#providers/SkuListProvider"
@@ -11,6 +12,7 @@ interface Props {
 }
 
 export const VariantSelector: FC<Props> = ({ variants, sku, setSku }) => {
+  const { t } = useTranslation()
   const options = variants.map((variant) => [variant.id, variant.name])
   const [value, setValue] = useState(sku.code)
   const onSkuChangeHandler = (e: ChangeEvent<HTMLSelectElement>) => {
@@ -23,7 +25,7 @@ export const VariantSelector: FC<Props> = ({ variants, sku, setSku }) => {
 
   return options.length > 0 ? (
     <div>
-      <Label>Colore</Label>
+      <Label>{t("labels.variant")}</Label>
       <Select
         value={value}
         onChange={onSkuChangeHandler}
